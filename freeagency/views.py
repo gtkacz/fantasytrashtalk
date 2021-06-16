0be1316c0b428f6ac87444a024b08d1931bc4d92
Tkacz <gabrielmt2@al.insper.edu.br>
2021-06-16 17:01:50 -0300

    a

--- freeagency/views.py
+++ freeagency/views.py
@@ -26,19 +26,19 @@ def index(request):
     players = FreeAgent.objects.all().order_by('last_name')
     return render(request, 'freeagency/index.html', {'players': players})
 
-def login(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'freeagency/login.html', {'form': form})
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('index')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'freeagency/login.html', {'form': form})
 
 # @login_required
 def playerview(request, name):
